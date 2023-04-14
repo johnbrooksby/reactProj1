ccb6dc08797e4541e24679db745aa9aa5bf94d9e
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-04-14 12:39:16 -0600

    All features work as desired. May attempt implementing a win counter later

--- src/App.js
+++ src/App.js
@@ -6,14 +6,65 @@ function App() {
   const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState(true);
 
+  const resetHandler = () => {
+    setSquares(["", "", "", "", "", "", "", "", ""]);
+    setPlayer(true);
+  };
+
+  const calculateWinner = (Arr) => {
+    let lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+
+      if (Arr[a] && Arr[a] === Arr[b] && Arr[a] === Arr[c]) {
+        return `${Arr[a]} won!`;
+      }
+    }
+    if (
+      !Arr[0] ||
+      !Arr[1] ||
+      !Arr[2] ||
+      !Arr[3] ||
+      !Arr[4] ||
+      !Arr[5] ||
+      !Arr[6] ||
+      !Arr[7] ||
+      !Arr[8]
+    ) {
+      return "Who will win?";
+    }
+    return "Cat's game";
+  };
+
   return (
     <div className="App">
-      <Square
-        squares={squares}
-        setSquares={setSquares}
-        player={player}
-        setPlayer={setPlayer}
-      />
+      <div className="container">
+        {squares.map((value, index) => {
+          return (
+            <Square
+              setSquares={setSquares}
+              index={index}
+              squareValue={value}
+              squares={squares}
+              player={player}
+              setPlayer={setPlayer}
+            />
+          );
+        })}
+      </div>
+      <span>{calculateWinner(squares)}</span>
+      <br />
+      <button onClick={resetHandler}>Reset</button>
     </div>
   );
 }
